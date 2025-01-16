0530b0b8ec33d918dac8177e323c146f3e87bfd5
preska <wolfwar@outlook.com>
2025-01-16 14:47:00 +0100

    init

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Posts = () => {
+  return (
+    <main className="flex flex-col gap-y-7 items-center text-center pt-24 ">
+      <h1 className="text-2xl sm:text-3xl font-semibold">Posts (0)</h1>
+      <ul className="border-t border-b border-black/10 leading-8 py-5"></ul>
+    </main>
+  );
+};
+
+export default Posts;
